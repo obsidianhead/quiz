7d79fe7e77607a8e0b90a159722a5a36c3147b40
Daniel Wright <obsidianhead@gmail.com>
2024-09-13 10:36:22 -0400

    build v12

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let selectedChapterName = '';
 const dbName = 'quiz';  // Database name
 const blobBaseUrl = 'https://quizstore.blob.core.windows.net/database';  // Base URL for the blob storage
 const localBaseUrl = './db';  // Base URL for local testing (local folder path)
-let isLocalMode = true;  // Set to 'true' for local testing, 'false' for Azure Blob
+let isLocalMode = false;  // Set to 'true' for local testing, 'false' for Azure Blob
 const forceDBDownload = true;  // Toggle between force download or use of IndexedDB
 
 // Open or create the IndexedDB
